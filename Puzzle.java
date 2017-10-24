b406296c68274c43b1cd4418eef09a88b7fe9b71
Nashiha Ahmed <nashiha.ahmed@yahoo.com>
2017-10-24 23:53:41 +0300

    Add files via upload

--- Puzzle.java
+++ Puzzle.java
@@ -5,6 +5,8 @@ import java.util.*;
  */
 public class Puzzle
 {
+    int x = 2;
+    int y = 2;
     private int[][] puzzle;
     Puzzle()
     {
@@ -37,18 +39,114 @@ public class Puzzle
     {
         this.puzzle = newPuzzle;
     }
-
+    public boolean goUp()
+    {
+        if( y == 0)
+        {
+            return false;
+        }
+        int temp = this.puzzle[y - 1][x];
+        this.puzzle[y - 1][x] = -1;
+        this.puzzle[y][x] = temp;
+        y = y - 1;
+        return true;
+    }
+    public boolean goDown()
+    {
+        if( y == 2)
+        {
+            return false;
+        }
+        int temp = this.puzzle[y + 1][x];
+        this.puzzle[y + 1][x] = -1;
+        this.puzzle[y][x] = temp;
+        y = y + 1;
+        return true;
+    }
+    public boolean goRight()
+    {
+        if( x == 2)
+        {
+            return false;
+        }
+        int temp = this.puzzle[y][x + 1];
+        this.puzzle[y][x + 1] = -1;
+        this.puzzle[y][x] = temp;
+        x = x + 1;
+        return true;
+    }
+    public boolean goLeft()
+    {
+        if( x == 0)
+        {
+            return false;
+        }
+        int temp = this.puzzle[y][x - 1];
+        this.puzzle[y][x - 1] = -1;
+        this.puzzle[y][x] = temp;
+        x = x - 1;
+        return true;
+    }
     public void puzzleGenerator()
     {
-        int[][] puzzleInstance = new int[3][3];
-        for( int i = 0; i < 3; i++)
+        Random randomGenerator = new Random();
+        int lastMove = -1;
+        for( int i = 0; i < 50; i++)
         {
-            for( int j = 0; j < 3; j++)
+            int randomInt = randomGenerator.nextInt(4);
+            int temp = 0;
+            //0 means up, 1 means down, 2 means right, 3 means left
+            if( randomInt == 0) //go up
             {
-                puzzleInstance[i][j] = 0;
+                if( lastMove == 1 )
+                {
+                    i--;
+                }
+                else if ( !(this.goUp()))
+                {
+                    i--;
+                }
             }
+            else if( randomInt == 1) //go down
+            {
+                if( lastMove == 0 )
+                {
+                    i--;
+                }
+                else if ( !this.goDown())
+                {
+                    i--;
+                }
+            }
+            else if( randomInt == 2)  //go right
+            {
+                if( lastMove == 3 )
+                {
+                    i--;
+                }
+               else if( !this.goRight())
+               {
+                    i--;
+               }
+            }
+            else if( randomInt == 3) //go left
+            {
+                if( lastMove == 2 )
+                {
+                    i--;
+                }
+                else if( !this.goLeft())
+                {
+                    i--;
+                }
+            }
+            else
+            {
+                System.out.println( "It can't go there!");
+            }
+            lastMove = randomInt;
+            //System.out.println( "i:" + i + "\n" + this.toString());
         }
-        this.setPuzzleArr( puzzleInstance);
     }
 
     public String toString()
@@ -70,10 +168,16 @@ public class Puzzle
     //will provide listings of program
     public static void main( String[] args)
     {
-        Puzzle puzzle;
-        puzzle = new Puzzle();
-        System.out.println( puzzle.toString());
-        puzzle.puzzleGenerator();
-        System.out.println( puzzle.toString());
+        Puzzle goalPuzzle;
+        goalPuzzle = new Puzzle();
+        System.out.println( "Goal state:\n" + goalPuzzle.toString());
+        Puzzle[] puzzleList = new Puzzle[10];
+        for( int i = 0; i < 10; i++)
+        {
+            puzzleList[i] = new Puzzle();
+            puzzleList[i].puzzleGenerator();
+            System.out.println( "Puzzle " + (i + 1) + ":\n" + puzzleList[i].toString());
+        }
+
     }
 }

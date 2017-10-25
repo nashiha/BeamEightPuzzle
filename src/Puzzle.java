86714d1b33589f34b6e4882236a4e23824c6b035
nashiha <nashiha.ahmed@yahoo.com>
2017-10-25 14:20:02 +0300

    removed ridiculous comments by chylpyl

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -11,11 +11,6 @@ public class Puzzle
     int y = 2;
     private int[][] puzzle;
     Puzzle()
-            ////
-            ////
-            ////
-            ////
-            /////
     {
         this.puzzle = new int[3][3];
         for( int i = 0; i < 3; i++)
